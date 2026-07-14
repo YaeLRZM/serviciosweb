f607654d57de20bb27528521a54d47e1760be485
Yael <yael.agustin.martinez.ramirez@gmail.com>
2026-07-14 11:54:11 -0600

    error con - en request

--- app/Http/Requests/StoreCuponCanjeadoRequest.php
+++ app/Http/Requests/StoreCuponCanjeadoRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreCupon_CanjeadoRequest extends FormRequest
+class StoreCuponCanjeadoRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- app/Http/Requests/StoreDetalleVentaRequest.php
+++ app/Http/Requests/StoreDetalleVentaRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreDetalle_VentaRequest extends FormRequest
+class StoreDetalleVentaRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
