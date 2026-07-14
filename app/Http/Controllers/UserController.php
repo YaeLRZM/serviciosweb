b7cc3e1b115ce65461f0e90ded1def0c1d3eb34b
crstvbl <zaito.zd@gmail.com>
2026-07-14 11:33:34 -0600

    feat: Add user management functionality with UserController, requests, policy, and migrations for roles and permissions

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $user = User::create($request->validated());
+        return response()->json(['message'=>'Usuario creado correctamente','usuario'=>$usuario],201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $user->update($request->validated());
+        return response()->json(['message'=>'Usuario actualizado correctamente','usuario'=>$user],201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json(['message' => 'Usuario eliminado correctamente']);
+    }
+}
